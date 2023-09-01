8be4af3a756483189410d4adce3d310084c02c03
qist <87984115@qq.com>
2023-09-01 17:04:33 +0800

    fix bug

--- js/dydhhy.js
+++ js/dydhhy.js
@@ -1,39 +1,39 @@
 var rule = {
-    title: 'dydhhy',
-    host: 'http://www.dydhhy.com',
-    homeUrl: '/',
-    url: '/tag/fyclass/page/fypage?',
-    filter_url: '{{fl.class}}',
-    filter: {},
-    searchUrl: '/?s=**',
-    searchable: 2,
-    quickSearch: 1,
-    filterable: 0,
-    headers: {
-        'User-Agent': 'MOBILE_UA',
-        'Cookie': ''
-    },
-    timeout: 5000,
-    class_name: '电视剧&电影&美剧&韩剧&日剧&英剧&2023&2022&2021',
-    class_url: 'tv&movie&美剧&韩剧&日剧&英剧&2023&2022&2021',
-    play_parse: true,
-    play_json: [{
-        re: '*',
-        json: {
-            parse: 0,
-            jx: 0
-        }
-    }],
-    lazy: '',
-    limit: 6,
-    推荐: 'div.clear:gt(1):has(img);.entry-title&&Text;img&&src;;a&&href',
-    一级: 'div.clear:gt(1):has(img);.entry-title&&Text;img&&src;;a&&href',
-    二级: {
-        title: ".single-excerpt&&Text",
-        img: "img&&src",
-        desc: ".entry-date&&Text",
-        content: "p&&Text",
-        tabs: `js: pdfh = jsp.pdfh;
+	title: 'dydhhy',
+	host: 'http://www.dydhhy.com',
+	homeUrl: '/',
+	url: '/tag/fyclass/page/fypage?',
+	filter_url: '{{fl.class}}',
+	filter: {},
+	searchUrl: '/?s=**',
+	searchable: 2,
+	quickSearch: 1,
+	filterable: 0,
+	headers: {
+		'User-Agent': 'MOBILE_UA',
+		'Cookie': ''
+	},
+	timeout: 5000,
+	class_name: '电视剧&电影&美剧&韩剧&日剧&英剧&2023&2022&2021',
+	class_url: 'tv&movie&美剧&韩剧&日剧&英剧&2023&2022&2021',
+	play_parse: true,
+	play_json: [{
+		re: '*',
+		json: {
+			parse: 0,
+			jx: 0
+		}
+	}],
+	lazy: '',
+	limit: 6,
+	推荐: 'div.clear:gt(1):has(img);.entry-title&&Text;img&&src;;a&&href',
+	一级: 'div.clear:gt(1):has(img);.entry-title&&Text;img&&src;;a&&href',
+	二级: {
+		title: ".single-excerpt&&Text",
+		img: "img&&src",
+		desc: ".entry-date&&Text",
+		content: "p&&Text",
+		tabs: `js: pdfh = jsp.pdfh;
         pdfa = jsp.pdfa;
         pd = jsp.pd;
 TABS=[]
@@ -75,7 +75,7 @@ tabsq.forEach(function(it){
 	tmpIndex = tmpIndex + 1;
 });
 log('xzys TABS >>>>>>>>>>>>>>>>>>' + TABS);`,
-lists: `js: log(TABS);
+		lists: `js: log(TABS);
 pdfh = jsp.pdfh;
 pdfa = jsp.pdfa;
 pd = jsp.pd;
@@ -138,5 +138,5 @@ listq.forEach(function(it){
 
 });`,
 
-}, 搜索: 'div.clear:gt(0):has(img);img&&alt;img&&data-src;;a&&href',
+	}, 搜索: 'div.clear:gt(0):has(img);img&&alt;img&&data-src;;a&&href',
 }
