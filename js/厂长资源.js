d616b5bd21a6542b609e0bd3e427cd42c61230fd
qist <87984115@qq.com>
2023-12-07 19:52:29 +0800

    update FM

--- js/厂长资源.js
+++ js/厂长资源.js
@@ -1,7 +1,7 @@
 var rule={
     title:'厂长资源',
     // host:'https://czspp.com', 
-    host:'https://www.czzy88.com',
+    host:'https://www.czzy.site',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});HOST = jsp.pdfh(html,"h3&&a&&href")',
     url:'/fyclassfyfilter',
     filterable:1,//是否启用分类筛选,
