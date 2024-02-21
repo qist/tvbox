7207ca8861da9caafe066830fa108ab6c8c73178
qist <87984115@qq.com>
2024-02-21 10:37:23 +0800

    update FM

--- js/农民影视.js
+++ js/农民影视.js
@@ -3,7 +3,7 @@ var rule={
 	title:'农民影视',
 	//host:'https://www.nmddd.com',
 	host:'https://www.nmdvd.com/',
-	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});let src = jsp.pdfh(html,"body&&a:eq(1)&&href");print(src);HOST=src',
+	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});let src = jsp.pdfh(html,"body&&a:eq(0)&&href");print(src);HOST=src',
 	url:'/vod-list-id-fyfilter.html',
 	// /vod-list-id-2-pg-1-order--by-time-class-0-year-2023-letter--area--lang-.html
 	filterable:1,//是否启用分类筛选,
