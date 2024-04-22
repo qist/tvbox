9a587a2158a142471003bf628063fcb249e21cfe
qist <87984115@qq.com>
2024-04-22 15:44:51 +0800

    update CAT

--- cat/tjs/js/changzhang.js
+++ cat/tjs/js/changzhang.js
@@ -32,7 +32,7 @@ function cryptJs(text, key, iv, type) {
 class ChangZhangSpider extends Spider {
     constructor() {
         super();
-        this.siteUrl = "https://www.czzy88.com"
+        this.siteUrl = "https://www.cz233.com"
     }
 
     async init(cfg) {
