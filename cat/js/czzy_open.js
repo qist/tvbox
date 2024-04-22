2ed4d980ae25326c0c7b9e0b3f61f76da718cd5c
qist <87984115@qq.com>
2024-04-22 09:56:57 +0800

    update FTY

--- cat/js/czzy_open.js
+++ cat/js/czzy_open.js
@@ -2,7 +2,7 @@
 import { Crypto, load, _ } from 'assets://js/lib/cat.js';
 
 let key = 'czzy';
-let host = 'https://cz01.vip/'; // 厂长地址发布页
+let host = 'https://www.cz233.com'; // 厂长地址发布页
 let url = '';
 let siteKey = '';
 let siteType = 0;
