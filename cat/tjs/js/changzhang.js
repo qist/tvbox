eb1535a74c73e991ad3af3cc7db28cef30dc8c67
qist <87984115@qq.com>
2024-05-24 10:02:48 +0800

    update 厂长

--- cat/tjs/js/changzhang.js
+++ cat/tjs/js/changzhang.js
@@ -32,7 +32,7 @@ function cryptJs(text, key, iv, type) {
 class ChangZhangSpider extends Spider {
     constructor() {
         super();
-        this.siteUrl = "https://www.czys.top"
+        this.siteUrl = "https://czzy.top"
     }
 
     async init(cfg) {
