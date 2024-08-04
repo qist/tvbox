639d54b8680993302775b7c681290ea674c10b05
qist <87984115@qq.com>
2024-08-04 13:08:55 +0800

    多渠道搜索

--- js/caiji.js
+++ js/caiji.js
@@ -1,28 +1,28 @@
-globalThis.getHeaders= function(input){
-	let headers = {
+globalThis.getHeaders = function (input) {
+    let headers = {
         'User-Agent': 'okhttp/4.1.0'
-	};
-	return headers
+    };
+    return headers
 }
 var rule = {
-	title:'点播',
-	host:'http://tv.jsp47.com',
-	homeUrl:'',
-    searchUrl:'https://search.video.iqiyi.com/o?if=html5&key=**&pageNum=fypage&pos=1&pageSize=25&site=iqiyi',
-    searchable:2,
-	quickSearch:1,
-	multi:1,
-    filterable:1,
-    headers:{
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.289 Safari/537.36'
+    title: '点播',
+    host: 'http://tv.jsp47.com',
+    homeUrl: '',
+    searchUrl: 'https://www.tycng.com/cj.php?q=**&pageNum=fypage',
+    searchable: 2,
+    quickSearch: 1,
+    multi: 1,
+    filterable: 1,
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.289 Safari/537.36'
     },
-    timeout:5000,
-    limit:20,	
-	play_parse:true, 
-	lazy:"", 
-    推荐:"",
-    一级:"",
-    二级:`js:
+    timeout: 5000,
+    limit: 20,
+    play_parse: true,
+    lazy: "",
+    推荐: "",
+    一级: "",
+    二级: `js:
         let d = [];
         try {               
             let html = request(input, {
@@ -47,7 +47,8 @@ var rule = {
                 VOD.vod_content = it.content ? it.content : "特别提醒:ㅤ友情提示您请勿相信影片中的广告，以免上当受骗";
                 VOD.vod_year = it.year;
                 VOD.vod_pic = it.pic ? it.pic : "http://114.100.48.52:18008/movjpg/" + it.name + ".jpg";
-                let names = it.name +"  ("+ (index+1) +")";
+                //let names = it.name +"  ("+ (index+1) +")";
+                let names = it.name;
                 let playEsp = it.source.eps;
                 playEsp.forEach(function(it) {
                     let source = names;
@@ -71,22 +72,19 @@ var rule = {
         } catch (e) {
             log('获取二级详情页发生错误:' + e.message)
         }`,
-    搜索:`js:
+    搜索: `js:
 		let d = [];
 		let html = request(input);
 		let json = JSON.parse(html);
-		json.data.docinfos.forEach(function(data) {
-            let channelName = data.albumDocInfo.channel.split(',')[0];
-            if (channelName.includes('电影') || channelName.includes('电视剧') || channelName.includes('综艺') || channelName.includes('动漫') || channelName.includes('少儿')) {
-                d.push({
-                    url: "https://www.tycng.com/caiji.php?jx=" + data.albumDocInfo.albumTitle,
-                    title: data.albumDocInfo.albumTitle,
-                    img: data.albumDocInfo.albumVImage,
-                    content:data.albumDocInfo.channel,
-                    desc: data.albumDocInfo.tvFocus
-                })
-            }
+		json.data.forEach(function(data) {
+            d.push({
+                url: "https://www.tycng.com/cj.php?id=" + data.id + "&port=" + json.port,
+                title: data.name,
+                img: data.pic,
+                content:data.content,
+                desc: data.remarks + "," + data.type_name + "," + json.port
+            })
 		});
 		setResult(d);
-	`    
+	`
 }
