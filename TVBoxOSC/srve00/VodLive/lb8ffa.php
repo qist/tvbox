41c1a9817e0ff4135ee544f40b80ed0b92787059
Bob Yang <bobyang3@gmail.com>
2024-11-22 22:53:46 -0800

    u

--- TVBoxOSC/srve00/VodLive/lb8ffa.php
+++ TVBoxOSC/srve00/VodLive/lb8ffa.php
@@ -8,7 +8,9 @@ if (strpos($userAgent, 'okhttp') === false) {
     echo "<br><br><br><br><br>Invalid player detected. Please use FongMi TV or OKTV on Android mobile devices or Android TV.";
 } else {
     //header("Location: https://ghp.ci/https://raw.githubusercontent.com/bobyang3/tvbox/own/TVBoxOSC/llist.txt");
-    header("Location: https://RobYang.serv00.net/110924.txt");
+    //header("Location: https://RobYang.serv00.net/110924.txt");
+    header("Location: https://robyang.us.kg/VodLive/jb8ffa");
+   
     exit;
 
 
