d445ac15e622841e3df1152c627cff0bc1e67b71
qist <juestnow@gmail.com>
2025-08-09 07:55:39 +0800

    Update 678.js

--- js/678.js
+++ js/678.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'闪雷影视',
     编码:'gb2312',
-    host:'http://211.143.79.104:82',
+    host:'http://121.16.47.94:5003',
     url:'/www/List.asp?classid=fyclass&searchword=&page=fypage',
     filterable:0,//是否启用分类筛选,
     class_name:'电影&电视剧&综艺&动漫&音乐',
