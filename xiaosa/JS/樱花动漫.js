da91e74a46135b41d996efd6ecc2339895424558
IsayIsee <solin.zhan@gmail.com>
2025-12-10 07:09:42 +0000

    清理冲突目录

--- xiaosa/JS/樱花动漫.js
+++
@@ -1,8 +0,0 @@
-var rule = {
-    模板: '首图',
-    title: '樱花动漫[漫]',
-    host: 'https://katedm.com/',
-    url: '/list/fyclass-fypage.html',
-    searchUrl: '/search/**----------fypage---.html',
-    搜索: '#searchList li;a&&title;.lazyload&&data-original;.pic-tag&&Text;a&&href',
-}
